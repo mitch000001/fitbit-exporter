cb191596e427817998ca65f82bfd3493287ffcd9
Michael Wagner <mitch.wagna@gmail.com>
2021-05-31 21:53:25 +0200

    chore(auth): implement basic auth flow

--- go.mod
+++ go.mod
@@ -3,6 +3,20 @@ module github.com/mitch000001/fitbit-exporter
 go 1.16
 
 require (
+	github.com/ddollar/forego v0.16.1 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/gobuffalo/envy v1.6.3 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/markbates/going v1.0.0 // indirect
+	github.com/markbates/grift v0.0.0-20180319170132-76f93617a788 // indirect
+	github.com/markbates/refresh v1.11.1 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/goreman v0.3.7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/cobra v1.1.3 // indirect
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
+	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,37 +8,48 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/fitbit"
 )
 
 func main() {
-	conf := &oauth2.Config{
-		ClientID:     os.Getenv("CLIENT_ID"),
-		ClientSecret: os.Getenv("CLIENT_SECRET"),
-		Scopes: []string{
-			"activity",
-			"heartrate",
-			"location",
-			"nutrition",
-			"profile",
-			"settings",
-			"sleep",
-			"social",
-			"weight",
+	conf := &OauthConfig{
+		State: uuid.NewString(),
+		Config: &oauth2.Config{
+			ClientID:     os.Getenv("OAUTH2_CLIENT_ID"),
+			ClientSecret: os.Getenv("OAUTH2_CLIENT_SECRET"),
+			Scopes: []string{
+				"activity",
+				"heartrate",
+				"location",
+				"nutrition",
+				"profile",
+				"settings",
+				"sleep",
+				"social",
+				"weight",
+			},
+			Endpoint: fitbit.Endpoint,
 		},
-		Endpoint: fitbit.Endpoint,
+		AuthCode: os.Getenv("OAUTH2_AUTH_CODE"),
 	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/auth", oauthHandler(conf))
-	mux.HandleFunc("/authorize", oauthHandler(conf))
+	mux.HandleFunc("/authorize", authorizeHandler(conf))
+	mux.HandleFunc("/oauth-redirect", oauthRedirectHandler(conf))
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Not found", http.StatusNotFound)
+	})
 	mux.HandleFunc("/", handler(conf))
 	server := &http.Server{
 		Addr:    ":3000",
@@ -74,42 +85,47 @@ func main() {
 
 }
 
-func handler(config *oauth2.Config) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		// Redirect user to consent page to ask for permission
-		// for the scopes specified above.
-		url := config.AuthCodeURL("state", oauth2.AccessTypeOffline)
-		fmt.Printf("Visit the URL for the auth dialog: %v", url)
-		// tok, err := config.Token(ctx)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// log.Printf("Token: %+#v\n", tok)
-		// client := oauth2.NewClient(ctx, config.TokenSource(ctx))
-		// client := oauth2.NewClient(r.Context())
+func handler(config *OauthConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !config.IsAuthorized() {
+			http.Redirect(w, r, "/auth", http.StatusTemporaryRedirect)
+			return
+		}
+		tok, err := config.Exchange(r.Context(), config.AuthCode, oauth2.AccessTypeOffline)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error exchanging token: %v", err), http.StatusInternalServerError)
+			return
+		}
+		log.Printf("Token: %+#v\n", tok)
+		client := config.Client(r.Context(), tok)
 
-		// res, err := client.PostForm("https://api.fitbit.com/1.1/oauth2/introspect", url.Values{"token": []string{tok.AccessToken}})
-		// if err != nil {
-		// 	log.Printf("Error introspecting token: %v", err)
-		// 	os.Exit(1)
-		// }
-		// if err := printResponse(res); err != nil {
-		// 	log.Printf("Error reading token introspect response: %v", err)
-		// 	os.Exit(1)
-		// }
-		// res, err := client.Get("https://api.fitbit.com/1/user/-/profile.json")
-		// if err != nil {
-		// 	log.Printf("Error getting devices: %v", err)
-		// 	os.Exit(1)
-		// }
-		// if err := printResponse(res); err != nil {
-		// 	log.Printf("Error reading devices: %v", err)
-		// 	os.Exit(1)
-		// }
+		res, err := client.PostForm("https://api.fitbit.com/1.1/oauth2/introspect", url.Values{"token": []string{tok.AccessToken}})
+		if err != nil {
+			log.Printf("Error introspecting token: %v", err)
+		}
+		if err := printResponse(res); err != nil {
+			log.Printf("Error reading token introspect response: %v", err)
+		}
+		res, err = client.Get("https://api.fitbit.com/1/user/-/profile.json")
+		if err != nil {
+			log.Printf("Error getting devices: %v", err)
+		}
+		if err := printResponse(res); err != nil {
+			log.Printf("Error reading devices: %v", err)
+		}
+		fmt.Fprint(w, `<!DOCTYPE html>
+        <html>
+            <body>
+                <h1>Fitbit exporter</h1>
+                <p>You successfully authorized this exporter to fetch your data from Fitbit</p>
+                <p>Visit the metrics endpoint at <a href="/metrics">/metrics</a></p>
+            </body>
+        </html>
+        `)
 	}
 }
 
-func oauthHandler(config *oauth2.Config) http.HandlerFunc {
+func oauthHandler(config *OauthConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := authTemplate.Execute(w, "auth.tpl.html"); err != nil {
 			http.Error(w, fmt.Sprintf("error while executing template: %v", err), http.StatusInternalServerError)
@@ -117,16 +133,44 @@ func oauthHandler(config *oauth2.Config) http.HandlerFunc {
 	}
 }
 
-func authorizeHandler() http.HandlerFunc {
+func authorizeHandler(config *OauthConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := r.ParseForm(); err != nil {
 			http.Error(w, fmt.Sprintf("error parsing form: %v", err), http.StatusBadRequest)
 		}
+		redirectURL, err := url.Parse(r.Form.Get("redirectURL"))
+		if err != nil {
+			log.Printf("Error parsing redirect URL: %v", err)
+		} else {
+			redirectURL.Path = "/oauth-redirect"
+			config.RedirectURL = redirectURL.String()
+		}
+		fitbitURL := config.AuthCodeURL(config.State, oauth2.AccessTypeOffline)
+		http.Redirect(w, r, fitbitURL, http.StatusTemporaryRedirect)
+	}
+}
 
+func oauthRedirectHandler(config *OauthConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := r.URL.Query()
+		authCode := params.Get("code")
+		state := params.Get("state")
+		if !config.IsStateValid(state) {
+			http.Error(w, "State does not match", http.StatusBadRequest)
+			return
+		}
+		config.AuthCode = authCode
+		templateValues := map[string]interface{}{
+			"scopes": config.Scopes,
+		}
+		if err := oauthRedirectTemplate.ExecuteTemplate(w, "oauth-redirect.tpl.html", templateValues); err != nil {
+			http.Error(w, fmt.Sprintf("error while executing template: %v", err), http.StatusInternalServerError)
+		}
 	}
 }
 
 var authTemplate = template.Must(template.ParseFiles("./auth.tpl.html"))
+var oauthRedirectTemplate = template.Must(template.ParseFiles("./oauth-redirect.tpl.html"))
 
 type logTransport struct {
 	transport http.RoundTripper
@@ -154,4 +198,21 @@ func printResponse(response *http.Response) error {
 	}
 	log.Printf("Response: \n%s\n", string(body))
 	return nil
+}
+
+type OauthConfig struct {
+	*oauth2.Config
+	AuthCode string
+	State    string
+	sync.Mutex
+}
+
+func (o OauthConfig) IsAuthorized() bool {
+	o.Lock()
+	defer o.Unlock()
+	return o.AuthCode != ""
+}
+
+func (o OauthConfig) IsStateValid(state string) bool {
+	return o.State == state
 }
